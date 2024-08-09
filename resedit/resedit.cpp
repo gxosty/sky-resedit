263c6a8334481a7c029bd32e1fed20f4d4aa9be2
gxosty <6321b5arsi@gmail.com>
2024-08-09 15:11:41 +0500

    Fixed typos. Added `std::promise` to hook

--- resedit/resedit.cpp
+++ resedit/resedit.cpp
@@ -4,28 +4,40 @@
 #include <Cipher/Cipher.h>
 #include <misc/Logger.h>
 
+#include <future>
+
 namespace resedit
 {
 	core::ResourcePackManager _resource_pack_manager;
-	CipherBase* _vfx_readfile_cipher_hook = nullptr;
+	CipherBase* _vfs_readfile_cipher_hook = nullptr;
+	std::promise<void> _vfs_readfile_promise;
+	std::future<void> _vfs_readfile_future = _vfs_readfile_promise.get_future();
 
 	uint64_t (*_vfs_readfile_orig)(void* this_, char* buffer, uint64_t max_len);
 	uint64_t _vfs_readfile_hook(void* this_, char* buffer, uint64_t max_len)
 	{
+		_vfs_readfile_future.wait();
+
 		uint64_t written_len = _vfs_readfile_orig(this_, buffer, max_len);
 
-		LOGI("%s", *(const char**)this_);
+		AssetData asset_data{
+			std::string(*(const char**)this_),
+			buffer,
+			max_len,
+			&written_len
+		};
 
 		return written_len;
 	}
 
 	void initialize()
 	{
-		_vfx_readfile_cipher_hook = (new CipherHook())
+		_vfs_readfile_cipher_hook = (new CipherHook())
 			->set_Hook((uintptr_t)_vfs_readfile_hook)
 			->set_Callback((uintptr_t)&_vfs_readfile_orig)
 			->set_Address(Cipher::get_libBase() + 0x1AA171C, false)
 			->Fire();
-		return;
+
+		_vfs_readfile_promise.set_value();
 	}
 }
